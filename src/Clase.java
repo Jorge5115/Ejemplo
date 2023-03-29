8bd2a028c57b90534e05e5970ff65bdd9e094e51
Jorge <Jorge@DESKTOP-6O0CSEI>
2023-03-30 01:04:19 +0200

    Cuarto commit

--- src/Clase.java
+++ src/Clase.java
@@ -5,7 +5,25 @@
 /**
  * @author Jorge
  *
- */
+ */	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class Clase {
 
 	/**
@@ -13,9 +31,11 @@ public class Clase {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int num= 7;
-int num2=33;
-int num8=0;
+		int num= 7;
+		int num2=33;
+		int num8=0;
+		
+		int total = num+num2+num8;
 	}
 
 }
