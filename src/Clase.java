0402bd3c5415bbe1a75b16264d7261df84f06371
Jorge5115 <jorge.casanova.sanchez@gmail.com>
2023-03-30 00:13:33 +0200

    Comienzo del commit

---
+++ src/Clase.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jorge
+ *
+ */
+public class Clase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num= 7;
+	}
+
+}
